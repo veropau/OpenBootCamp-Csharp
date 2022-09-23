caeabdb7474fe1fa58d015dc5516443ce31adc0a
veropau <114253396+veropau@users.noreply.github.com>
2022-09-23 15:26:38 -0400

    Ejercicios sesión 1
    Ejercicio 1: 
    Escribe un programa que reciba tu nombre y lo escriba por consola.
    
    Ejercicio 2: 
    Escribe un programa que tome la hora y la escriba por pantalla.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Ejercicios sesión 1");
+Console.WriteLine();
+// Ejercicio 1
+// Escribe un programa que reciba tu nombre y lo escriba por consola.
+Console.WriteLine("Ejercicio 1 => Escribe un programa que reciba tu nombre y lo escriba por consola.");
+Console.WriteLine();
+Console.Write("Digite su nombre: ");
+var nameUser = Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine("Su nombre es: " + nameUser);
+Console.WriteLine();
+// Ejercicio 2
+// Escribe un programa que tome la hora y la escriba por pantalla.
+Console.WriteLine("Ejercicio 2 => Escribe un programa que tome la hora y la escriba por pantalla.");
+var hora = (DateTime.Now).ToString("hh:mm:ss");
+Console.WriteLine(hora);
