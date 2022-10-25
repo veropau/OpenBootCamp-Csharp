3583b3af1e7cf02f6e9a78d6ae53ea61ae16ba01
veropau <114253396+veropau@users.noreply.github.com>
2022-10-25 13:43:11 -0400

    Ejercicios-sesión-3
    Ejercicios-sesión-3 OpenBootcamp C#

--- Program.cs
+++ Program.cs
@@ -1,2 +1,109 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Ejercicios sesión 1");
+//Console.WriteLine();
+//// Ejercicio 1
+//// Escribe un programa que reciba tu nombre y lo escriba por consola.
+//Console.WriteLine("Ejercicio 1 => Escribe un programa que reciba tu nombre y lo escriba por consola.");
+//Console.WriteLine();
+//Console.Write("Digite su nombre: ");
+//var nameUser = Console.ReadLine();
+//Console.WriteLine();
+//Console.WriteLine("Su nombre es: " + nameUser);
+//Console.WriteLine();
+//// Ejercicio 2
+//// Escribe un programa que tome la hora y la escriba por pantalla.
+//Console.WriteLine("Ejercicio 2 => Escribe un programa que tome la hora y la escriba por pantalla.");
+//var hora = (DateTime.Now).ToString("hh:mm:ss");
+//Console.WriteLine(hora);
+
+
+
+//Console.WriteLine(" Ejercicios sesión 2");
+//Console.WriteLine();
+
+//Console.WriteLine(" Ejercicio 1 => Variables Escribe un programa que reciba datos de una persona y genera un mensaje, usa una variable para cada dato y otra para el mensaje. Ej: nombre, apellido, edad, sabe programar, etc.");
+//Console.WriteLine("");
+//Console.WriteLine(" Datos Persona");
+//Console.WriteLine("");
+//Console.Write(" Digite su nombre: ");
+//string nombre = Console.ReadLine();
+//Console.Write(" Digite su apellido: ");
+//string apellido = Console.ReadLine();
+//Console.Write(" Digite su edad: ");
+//string edad = Console.ReadLine();
+//Console.Write(" Sabe programar?: ");
+//string sabeProgramar = Console.ReadLine();
+//Console.WriteLine("");
+
+//string mensaje = $" Su nombre completo es {nombre} {apellido}, tiene {edad} años y {sabeProgramar} sabe programar";
+//Console.WriteLine(mensaje);
+
+
+//Console.WriteLine("Ejercicio 2");
+//Console.WriteLine("Tipos Usando los tipos de variables más adecuados, describe los objetos siguientes:");
+//Console.WriteLine();
+// Coche: puertas, ruedas, marca, ITV vigente
+// Mesa: peso, largo, material, color
+// Nota: puedes escribir estos datos por consola si prefieres verlos. La idea del ejercicio es almacenar los datos en los tipos más adecuados.
+
+//Console.WriteLine("Coche"); 
+//Console.WriteLine(" int puertas;");
+//Console.WriteLine(" int ruedas;");
+//Console.WriteLine(" string marca;");
+//Console.WriteLine(" bool ITVvigente;");
+
+//Console.WriteLine();
+//Console.WriteLine("Mesa");
+//Console.WriteLine(" double peso;");
+//Console.WriteLine(" double largo;");
+//Console.WriteLine(" string material;");
+//Console.WriteLine(" string color;");
+//Console.WriteLine();
+
+//Console.WriteLine(" Ejercicio 3");
+//Console.WriteLine(" Operadores Determina los operadores para verificar las siguientes condiciones:");
+//Console.WriteLine();
+//Console.WriteLine(" Un número es mayor o igual a 18");
+//Console.WriteLine(" x >= 18");
+//Console.WriteLine();
+//Console.WriteLine(" Un char es ‘a’");
+//Console.WriteLine(" char == 'a'");
+//Console.WriteLine();
+//Console.WriteLine(" Se cumplen dos condiciones a la vez (ambas verdaderas)");
+//Console.WriteLine(" (5>4) && (4<5)");
+//Console.WriteLine();
+//Console.WriteLine(" Se cumple una de dos condiciones a la vez (una true y otra false)");
+//Console.WriteLine(" (5<4) || (4<5)");
+// Nota: puedes escribir estos datos por consola si prefieres verlos. La idea del ejercicio es almacenar los datos en los tipos más adecuados.
+
+
+Console.WriteLine("Ejercicios sesión 3");
+Console.WriteLine();
+// Ejercicio 1
+Console.WriteLine("Ejercicio 1 => Crea una estructura de datos para un cliente con estos campos: Nombre completo, Teléfono, Dirección, Email");
+Console.WriteLine();
+// Si es nuevo cliente
+// Bonus: escribe un método para presentar estos datos desde la estructura al hacer Console.WriteLine(...)
+
+Clientes cliente1 = new Clientes("Maria Pulido", "809-758-6235", "c/guarocuya #52", "mariapulido@algo.com");
+Clientes cliente2 = cliente1 with { Nombre_Completo = "Carlos Perez", Email = "carlosperez@otro.com" };
+Console.WriteLine(cliente1);
+Console.WriteLine();
+Console.WriteLine(cliente2);
+public struct Clientes {
+
+    public Clientes(string nombre_completo, string telefono, string direccion, string email) {
+        Nombre_Completo = nombre_completo;
+        Telefono = telefono;
+        Direccion = direccion;
+        Email = email;
+    }
+
+    public string Nombre_Completo { get; set; }
+    public string Telefono { get; set; }
+    public string Direccion { get; set; }
+    public string Email { get; set; }
+
+    public override string ToString() => ($" El cliente {Nombre_Completo}, cuyo número de contacto es {Telefono}, vive en {Direccion} y tiene el correo {Email}");
+
+}
